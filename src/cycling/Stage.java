be04c7e176a966df799c2bdf209df5e4736906f5
Owen Lee <owentheturkey@gmail.com>
2022-03-02 13:26:14 +0000

    minimally compiling bs
    whatever

--- src/cycling/Stage.java
+++ src/cycling/Stage.java
@@ -1,5 +1,19 @@
 package cycling;
+import java.time.LocalDateTime;
 
 public class Stage {
-
+	
+	StageType stageType;
+	String stageName;
+	String stageDescription;
+	double stageLength;
+	LocalDateTime stageStartTime;
+	
+	public Stage(String name, String description, double length, LocalDateTime startTime, StageType type){
+		stageType = type;
+		stageName = name;
+		stageDescription = description;
+		stageLength = length;
+		stageStartTime= startTime;
+	}
 }
