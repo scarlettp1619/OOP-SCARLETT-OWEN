92e94481b74b5e53ed2d6adb0060f7ab0e82dafd
Owen Lee <owentheturkey@gmail.com>
2022-03-02 14:53:00 +0000

    fixed scars shit

--- src/cycling/Team.java
+++ src/cycling/Team.java
@@ -3,16 +3,10 @@ package cycling;
 public class Team {
 	String teamName;
 	String teamDescription;
-	int riderLength = 0;
-	//int teamID;
-	
-
-	//throws illegalnameexception if name already exists
-	//throws invalidnameexception if name is null
 	
 	public Team(String name, String description) {
-		name = teamName;
-		description = teamDescription;
+		teamName = name;
+		teamDescription = description;
 		
 	}
 }
